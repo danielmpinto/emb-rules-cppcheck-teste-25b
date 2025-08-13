5b6dd35c8519836dbb4bf8bf17557b5d5bfbc529
danielmpinto <danielmessias.engenharia@gmail.com>
2025-08-13 16:06:07 -0300

    teste sem erros

--- main.c
+++ main.c
@@ -4,16 +4,17 @@
 #define NUM_ELEMENTS 5
 
 int multiplyNumbers(int x, int y) {
-    int product = x * y;
+    return x * y;
 }
 
 int main(void) {
-    int a, b = 5; //uninitvar, legacyUninitvar
-    int result = multiplyNumbers(a, b);
+    //int a, b = 5; //uninitvar, legacyUninitvar
+    //int result = multiplyNumbers(a, b);
 
     int arr[NUM_ELEMENTS];
-    for(int i = 0; i <= NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
+    for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
         arr[i] = i;
+        printf("%d",arr[i]);
     }
 
     return 0;
